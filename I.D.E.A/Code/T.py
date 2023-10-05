dd78ae390584223f120afab88513538f59eb34b8
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-10-05 21:31:43 +0900

    Add files via upload
    C:\Users\12612\OneDrive\바탕 화면\학교\I.D.E.A>C:/Users/12612/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/12612/OneDrive/바탕 화면/학교/I.D.E.A/Code/T.py"
    Traceback (most recent call last):
      File "c:\Users\12612\OneDrive\바탕 화면\학교\I.D.E.A\Code\T.py", line 69, in <module>
        if y == repeat[rpnum1+1][0]:
    TypeError: 'NoneType' object is not subscriptable

--- I.D.E.A/Code/T.py
+++ I.D.E.A/Code/T.py
@@ -9,6 +9,10 @@ model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-
 df = pd.read_csv('C:/Users/12612/OneDrive/바탕 화면/학교/I.D.E.A/데이터셋/02)웰니스_대화_스크립트_데이터셋.csv')
 df = df.drop(columns=['임상질문그룹(연세의료원제공)', 'utterance(긍정)', 'utterance(부정)', '긍정에 대한 챗봇 답변', '부정에 대한 챗봇 답변', '추가발화(190917)', '추가발화 (191031)'])
 df = df[~df['response(공감)'].isna()]
+Nrow = pd.DataFrame(columns=df.columns)
+Nrow.loc[0]=[0,0,0,0,0,0,0,0]
+Ndf = pd.concat([Nrow, df, Nrow], ignore_index=True)
+Ndf = Ndf.fillna(0)
 
 #0번째 행 & 마지막 행에 빈칸 추가
 new_row = []
@@ -21,19 +25,18 @@ Ndf = pd.concat([df.iloc[:0], Nrow, df.iloc[0:]], ignore_index=True)
 Ndf = pd.concat([Nrow], ignore_index=True)
 
 def rp(l, col):
-      n = None
       nums = []
       nume = []
       numr = []
       numu = []
       
       #항 묶음 구하기
-      for a in range(len(l.index)):
-            if (l.iloc[a, l.columns[col]].isna() == False) & (l.iloc[a+1, l.columns[col]].isna() == True):
-                  nums.append(a)
-                        
-            if (l.iloc[a, l.columns[col]].isna() == True) & (l.iloc[a+1, l.columns[col]].isna == False):
-                  nume.append(a)
+      for a in range(len(l.index)-1):
+            if (l.iloc[a, col] == 0) & (l.iloc[a+1, col] != 0):
+                nums.append(a)
+                    
+            if (l.iloc[a, col] != 0) & (l.iloc[a+1, col] == 0):
+                nume.append(a)
 
       #항 개수 구하기               
       for b in range(len(nums)):
@@ -57,7 +60,7 @@ def rp(l, col):
 
 #묶음행에 빈칸이 있는 경우 다음 묶음행 전까지 묶음행을 반복해서 넣기
 for x in Ndf.columns:
-      repeat = rp(Ndf, [x])
+      repeat = rp(Ndf, x)
       rpnum1 = 0
       
       for y in range(len(Ndf.index)):
