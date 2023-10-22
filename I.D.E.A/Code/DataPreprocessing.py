cfc2d5b30573c8a2caf65c4a2d39ca688f514690
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-10-22 23:52:08 +0900

    Add files via upload

--- I.D.E.A/Code/DataPreprocessing.py
+++ I.D.E.A/Code/DataPreprocessing.py
@@ -1,11 +1,8 @@
 import pandas as pd
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-
-model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
 
 df = pd.read_csv('C:/Users/user/Desktop/Coding/Python/python_AI/I.D.E.A/I.D.E.A-main/I.D.E.A-main/I.D.E.A/데이터셋/02)웰니스_대화_스크립트_데이터셋.csv')
 df = df.drop(columns=['특이사항','utterance','임상질문그룹(연세의료원제공)', 'utterance(긍정)', 'utterance(부정)', '긍정에 대한 챗봇 답변', '부정에 대한 챗봇 답변', '추가발화(190917)', '추가발화 (191031)'])
+df = df[~df['response(공감)'].isna()]
 
 #0번째 행 & 마지막 행에 빈칸 추가
 Nrow = pd.DataFrame(columns=df.columns)
@@ -54,24 +51,19 @@ def rp(l, col):
       return numu
 
 print(Ndf)
-columns = ['핵심증상', 'intent', 'keyword(임상키워드)', '연관표현', 'response(공감)']
-print(columns)
-f = open("rpnum.txt","wt")
 
 #묶음행에 빈칸이 있는 경우 다음 묶음행 전까지 묶음행을 반복해서 넣기
-for x in range(len(columns)):
+for x in range(len(df.columns)):
       repeat = rp(Ndf, x)
       rpnum1 = 0 #밖 순차
       rpnum2 = -1 #안 순차
-
-      #print("rp(Ndf, x): "+str(repeat),"\n")
       
       for y in range(len(Ndf.index)):
             if rpnum1 < len(repeat)-1:
                   if (y == repeat[rpnum1+1][0]): #밖 다음 순차
                     rpnum1 += 1
                     rpnum2 = -1
-                            
+                              
                   if Ndf.iloc[y+1][x] == 0:
                         rw = int(y+1)
                         Nrw = int(repeat[rpnum1][rpnum2])
@@ -83,14 +75,9 @@ for x in range(len(columns)):
                         
                         e = repeat[rpnum1][-1] 
                         s = repeat[rpnum1][0]
+                        
                         if (e - s) != 0: #안 다음 순차
                               rpnum2 += 1
-            
-            print("rpnum1:",rpnum1)
-            print("rpnum2:",rpnum2,"\n")
-            lines = ["rpnum1:",str(rpnum1),"\nrpnum2:",str(rpnum2),"\n\n"]
-            f.writelines(lines)
-     
-print(Ndf)
+
 Ndf.to_csv("C:/Users/user/Desktop/Coding/Python/python_AI/I.D.E.A/I.D.E.A-main/I.D.E.A-main/I.D.E.A/데이터셋/Nndf.csv", index=False)
-f.close
+print(Ndf)
--- I.D.E.A/Code/main.py
+++ I.D.E.A/Code/main.py
@@ -1,14 +1,12 @@
 import pandas as pd
-from numpy import dot
-from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
 model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
 
-df = pd.read_csv('C:/Users/12612/OneDrive/바탕 화면/I.D.E.A/데이터셋/02)웰니스_대화_스크립트_데이터셋.csv')
-df = df.drop(columns=['임상질문그룹(연세의료원제공)', 'utterance(긍정)', 'utterance(부정)', '긍정에 대한 챗봇 답변', '부정에 대한 챗봇 답변', '추가발화(190917)', '추가발화 (191031)'])
-df = df[~df['response(공감)'].isna()]
+df = pd.read_csv('C:/Users/user/Desktop/Coding/Python/python_AI/I.D.E.A/I.D.E.A-main/I.D.E.A-main/I.D.E.A/데이터셋/Nndf.csv')
+#df = df.drop(columns=['임상질문그룹(연세의료원제공)', 'utterance(긍정)', 'utterance(부정)', '긍정에 대한 챗봇 답변', '부정에 대한 챗봇 답변', '추가발화(190917)', '추가발화 (191031)'])
+#df = df[~df['response(공감)'].isna()]
 
 df['embedding'] = df['utterance(2차)'].map(lambda x: list(model.encode(x)))
 
@@ -16,13 +14,10 @@ text = input('In: ')
 embedding = model.encode(text)
 
 df['similarity'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
-
-print(df.head()) 
-
 answer = df.loc[df['similarity'].idxmax()]
 
-a = ['핵심증상', 'intent', '특이사항', '연관표현', 'utterance(2차)', 'response(공감)', 'similarity']
-Output = ['핵심 증상', '구분', '특이사항', '연관표현', '유사한 발화', '응답', '유사도']
+a = ['intent', '연관표현', 'utterance(2차)', 'response(공감)', 'similarity']
+Output = ['구분', '연관표현', '유사한 발화', '응답', '유사도']
 
-for i in range(7):
-      print(Output[i] +":", str(answer[a[i]]) +"\n")
+for i in range(len(a)):
+      print("\n",Output[i] +":", str(answer[a[i]]))
