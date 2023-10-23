66a26135002b628822e5cd125c506a1a56ac9a11
KMS1105 <134215862+KMS1105@users.noreply.github.com>
2023-10-24 01:12:21 +0900

    Add files via upload

--- I.D.E.A/Code/app.py
+++ I.D.E.A/Code/app.py
@@ -1,31 +1,52 @@
 import pandas as pd
 import streamlit as st
+from streamlit_chat import message
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import json
 
-model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+@st.cache_resource
+def cashed_model():
+     model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+     return model
 
-st.title("감정 분석")
+@st.cache_data
+def get_dataset():
+     df = pd.read_csv('C:/Users/user/Desktop/Coding/Python/python_AI/I.D.E.A/I.D.E.A-main/I.D.E.A-main/I.D.E.A/데이터셋/Nndf.csv')
+     df['embedding'] = df['utterance(2차)'].apply(json.loads)
+     return df
 
-df = pd.read_csv('C:/Users/user/Desktop/Coding/Python/python_AI/I.D.E.A/I.D.E.A-main/I.D.E.A-main/I.D.E.A/데이터셋/Nndf.csv')
-#df = df.drop(columns=['임상질문그룹(연세의료원제공)', 'utterance(긍정)', 'utterance(부정)', '긍정에 대한 챗봇 답변', '부정에 대한 챗봇 답변', '추가발화(190917)', '추가발화 (191031)'])
-#df = df[~df['response(공감)'].isna()]
+model = cashed_model()
+df = get_dataset()
 
-df['embedding'] = df['utterance(2차)'].map(lambda x: list(model.encode(x)))
+st.header("감정 분석")
 
-with st.form("form"):
-    User_input = st.text_input("Prompt")
-    submit = st.form_submit_button("Submit")
+if 'generated' not in st.session_state:
+     st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+     st.session_state['past'] = []
+
+with st.form('form', clear_on_submit=True):
+     User_input = st.text_input("You: ", '')
+     submit = st.form_submit_button("Submit")
 
 if submit and User_input:
-      text = User_input
-      embedding = model.encode(text)
+      embedding = model.encode(User_input)
 
       df['similarity'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
       answer = df.loc[df['similarity'].idxmax()]
 
       a = ['intent', '연관표현', 'utterance(2차)', 'response(공감)', 'similarity']
-      Output = ['구분', '연관표현', '유사한 발화', '응답', '유사도']
-
+      #Output = ['구분', '연관표현', '유사한 발화', '응답', '유사도']
+      st.session_state.past.append(User_input)
+      
       for i in range(len(a)):
-            st.write(Output[i] +":", str(answer[a[i]]),"\n")
+            st.session_state.past.append(answer[a[i]])
+            #st.write(Output[i] +":", str(answer[a[i]]),"\n")
+
+for i in range(len(st.session_state['past'])):
+     message(st.session_state['past'][i], is_user=True, key=str(i)+'_user')
+     
+     if len(st.session_state['generated']) > i:
+          message(st.session_state['past'][i], is_user=True, key=str(i)+'_bot')
